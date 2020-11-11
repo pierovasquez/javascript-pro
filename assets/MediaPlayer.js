de08ba219d31dd3ee889defc744af8839ca8da7b
Piero Vasquez <piero.vasquez.r@gmail.com>
2020-11-11 22:08:14 +0100

    Mediaplayer file changed from being a javascript to a class just using visual studio recommendation tool

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,56 +1,58 @@
-function MediaPlayer(config) {
-  this.media = config.el;
-  this.plugins = config.plugins || [];
+class MediaPlayer {
+  constructor(config) {
+    this.media = config.el;
+    this.plugins = config.plugins || [];
 
-  this._initPlugins();
-}
-
-MediaPlayer.prototype._initPlugins = function() {
-  const player = {
-    playVideo: () => this.playVideo(),
-    pauseVideo: () => this.pauseVideo(),
-    media: this.media,
-    get muted() {
-      return this.media.muted;
-    },
-    set muted(value) {
-      this.media.muted = value;
-    }
+    this._initPlugins();
   }
-  this.plugins.forEach(plugin => {
-    plugin.run(player);
-  })
-}
-
-MediaPlayer.prototype.mute = function() {
-  this.media.muted = true;
-}
-
-MediaPlayer.prototype.unMute = function() {
-  this.media.muted = false;
-}
-MediaPlayer.prototype.playVideo = function () {
-  this.media.play();
-}
-
-MediaPlayer.prototype.pauseVideo = function () {
-  this.media.pause();
-}
-
-MediaPlayer.prototype.toggleVideo = function () {
-  if (this.media.paused) {
-    this.playVideo();
-  } else {
-    this.pauseVideo();
+  _initPlugins() {
+    const player = {
+      playVideo: () => this.playVideo(),
+      pauseVideo: () => this.pauseVideo(),
+      media: this.media,
+      get muted() {
+        return this.media.muted;
+      },
+      set muted(value) {
+        this.media.muted = value;
+      }
+    };
+    this.plugins.forEach(plugin => {
+      plugin.run(player);
+    });
   }
-}
-
-MediaPlayer.prototype.toggleMute = function() {
-  if (this.media.muted) {
-    this.media.muted = false;
-  } else {
+  mute() {
     this.media.muted = true;
   }
+  unMute() {
+    this.media.muted = false;
+  }
+  playVideo() {
+    this.media.play();
+  }
+  pauseVideo() {
+    this.media.pause();
+  }
+  toggleVideo() {
+    if (this.media.paused) {
+      this.playVideo();
+    } else {
+      this.pauseVideo();
+    }
+  }
+  toggleMute() {
+    if (this.media.muted) {
+      this.media.muted = false;
+    } else {
+      this.media.muted = true;
+    }
+  }
 }
+
+
+
+
+
+
 
 export default MediaPlayer;
--- ejercicios/typescript/index.ts
+++
@@ -1,7 +0,0 @@
-console.log('hello typescript')
-
-function add(a: number, b: number) {
-  return a + b;
-}
-
-const sum = add(2,3);
